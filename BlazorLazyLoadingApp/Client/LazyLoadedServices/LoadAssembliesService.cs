ede2a4bbce181cd3d558a2dfdf1a66536b1b25da
Kevin Lockerby <kleelockerby@gmail.com>
2022-11-29 11:55:30 -0500

    Created custom AssemblySettings.json

--- BlazorLazyLoadingApp/Client/LazyLoadedServices/LoadAssembliesService.cs
+++ BlazorLazyLoadingApp/Client/LazyLoadedServices/LoadAssembliesService.cs
@@ -14,26 +14,29 @@ namespace BlazorLazyLoadingApp.Client
 
         public IEnumerable<string> GetCounterAssemblies()
         {
-            IConfigurationSection fileNamesSection = Configuration.GetSection("CounterAssemblies");
+            IConfigurationSection fileNamesSection = Configuration.GetSection("AssembliesCounter");
             IEnumerable<KeyValuePair<string, string>> fileNamesArray = fileNamesSection.AsEnumerable();
 
-            IEnumerable<IConfigurationSection> fileNamesSection2 = Configuration.GetSection("CounterAssemblies").GetChildren();
+            IEnumerable<IConfigurationSection> fileNamesSection2 = Configuration.GetSection("AssembliesCounter").GetChildren();
             IEnumerable<string> retval = fileNamesSection2.ToArray().Select(c => c.Value).ToArray().AsEnumerable();
             return retval;
         }
 
         public IEnumerable<string> GetComponentRefAssemblies()
         {
-            IConfigurationSection fileNamesSection = Configuration.GetSection("ComponentRefAssemblies");
+            IConfigurationSection fileNamesSection = Configuration.GetSection("AssembliesComponentRef");
             IEnumerable<KeyValuePair<string, string>> fileNamesArray = fileNamesSection.AsEnumerable();
-            return Configuration.GetSection("CounterAssemblies").GetChildren().ToArray().Select(c => c.Value).ToArray().AsEnumerable();
+            return Configuration.GetSection("AssembliesComponentRef").GetChildren().ToArray().Select(c => c.Value).ToArray().AsEnumerable();
         }
-
+         
         public IEnumerable<string> GetWeatherAssemblies()
         {
-            IConfigurationSection fileNamesSection = Configuration.GetSection("WeatherAssemblies");
+            IConfigurationSection fileNamesSection = Configuration.GetSection("AssembliesWeather");
             IEnumerable<KeyValuePair<string, string>> fileNamesArray = fileNamesSection.AsEnumerable();
-            return Configuration.GetSection("CounterAssemblies").GetChildren().ToArray().Select(c => c.Value).ToArray().AsEnumerable();
+
+            IEnumerable<IConfigurationSection> fileNamesSection2 = Configuration.GetSection("AssembliesWeather").GetChildren();
+            IEnumerable<string> retval = fileNamesSection2.ToArray().Select(c => c.Value).ToArray().AsEnumerable();
+            return retval;
         }
     }
 }
--- BlazorLazyLoadingApp/Client/Program.cs
+++ BlazorLazyLoadingApp/Client/Program.cs
@@ -1,6 +1,7 @@
 using BlazorLazyLoadingApp.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
 using Syncfusion.Blazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -8,7 +9,17 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NjYxMjU2QDMyMzAyZTMxMmUzME81WGE2cDRKSnA2eUdFZFRlMFdsRGtGdVEzaVBwU0pzelBienNDQW5IUnc9");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+var http = new HttpClient()
+{
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+};
+builder.Services.AddScoped(sp => http);
+using var response = await http.GetAsync("assemblySettings.json");
+using var stream = await response.Content.ReadAsStreamAsync();
+builder.Configuration.AddJsonStream(stream);
+
 builder.Services.AddScoped<WeatherForecastLazyService>();
 builder.Services.AddSyncfusionBlazor();
 builder.Services.AddScoped<LoadAssembliesService>();
